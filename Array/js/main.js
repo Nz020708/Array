da803f4817305cdf7e8ee6a586c0d9f418790f2e
Naz Mzadeh <100687473+Nz020708@users.noreply.github.com>
2022-11-10 11:38:04 +0400

    Update main.js

--- Array/js/main.js
+++ Array/js/main.js
@@ -162,5 +162,19 @@ const arr=[50,7,90,11,46,9,2,39];
 // }
 // console.log(selectionSort(arr));
 
+function insertionSort(array) {
+    for (let i = 1; i < array.length; i++) {
+        let currentValue = array[i]
+        let j;
+        for ( j = i-1; j>=0 && array[j] > currentValue; j--) {
+            array[j+1]=array[j]
+            
+        }
+        array[j+1]=currentValue;
+        
+    }
+    return array;
+ }
+console.log(insertionSort(arr));
 
 //10th task
